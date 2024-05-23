715f10020e3b4b33740802bbf80a936318e5ae00
Ayafaraj20 <ayaehlees51@gmail.com>
2024-05-23 20:09:48 +0200

    log

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
+import 'package:alarm_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        //هنا لون الخلفية
-        backgroundColor: Colors.green[50],
-        appBar: AppBar(
-          title: Text('ALARM'),
-          //اسم التطبيق من فوق
-          backgroundColor: Colors.green[200],
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('assets/alarm.jpg'),
-            ),
-        ),
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: false,
       ),
-    ),
-  );
+      home: const SplashScreen(),
+    );
+  }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:alarm_app/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        title: const Text('ALARM'),
+        //اسم التطبيق من فوق
+        backgroundColor: Colors.green[200],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('assets/alarm.jpg'),
+        ),
+      ),
+    );
+  }
+}
