67ac85fb813a9fe2ef17a90a5691aa7fba8fc3d7
ghofran-altajory <167257184+ghofran-altajory@users.noreply.github.com>
2024-05-24 00:02:05 +0200

    12

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,6 +10,34 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white ,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+          //Image
+          Image.asset(
+            'assets/ss.png' , 
+            height: 150,
+          )
+          
+          //Title
+          
+          //sub title
+          
+          //user name
+          
+          //password
+          
+          //sign in
+          
+          //sign up
+              
+            ],),
+        ),
+      ),
+    );
   }
 }
