206c76ad6a1f563bf7666e752efaf64429c202f5
ghofran-altajory <167257184+ghofran-altajory@users.noreply.github.com>
2024-05-23 18:55:04 +0200

    حطينا صورة

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('hello')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        //هنا لون الخلفية
+        backgroundColor: Colors.green[50],
+        appBar: AppBar(
+          title: Text('ALARM'),
+          //اسم التطبيق من فوق
+          backgroundColor: Colors.green[200],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('assets/alarm.jpg'),
+            ),
+        ),
+      ),
+    ),
+  );
 }
