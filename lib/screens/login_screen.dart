3db479a740db35ece138d013b2ce511a12ff2f61
Ayafaraj20 <ayaehlees51@gmail.com>
2024-05-24 23:07:12 +0200

    d

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,7 +10,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   void openSignupScreen() {
-    Navigator.of(context).pushReplacement('SignupScreen' as Route<Object?>);
+    Navigator.of(context).pushReplacementNamed('signupScreen');
   }
 
   @override
