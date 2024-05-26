ec369db65bc11e774cab93bf531ebedaf7e6f665
Ayafaraj20 <ayaehlees51@gmail.com>
2024-05-26 21:34:54 +0200

    sing up

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -8,12 +10,189 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  void openSignupScreen() {
+    Navigator.of(context).pushReplacementNamed('signupScreen');
+  }
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController confirmpasswordcontroller = TextEditingController();
+  Future signUp() async {
+    if (passwordConfirmed()) {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailcontroller.text.trim(),
+        password: passwordcontroller.text.trim(),
+      );
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).pushNamed('/');
+    }
+  }
+
+  bool passwordConfirmed() {
+    if (passwordcontroller.text.trim() ==
+        confirmpasswordcontroller.text.trim()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-body: Center(
-  child: Text('شاشة التسجيل')),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(223, 242, 255, 233),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            //scrolling
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Image
+                //Image.asset(
+                //'assets/pic.png',
+                //height: 150,
+                //),
+                const SizedBox(height: 20),
+                //Title
+                Text(
+                  'التسجيل ',
+                  style: GoogleFonts.robotoCondensed(
+                      fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                //sub title
+                Text(
+                  'مرحبا!هنا بإمكانك التسجيل',
+                  style: GoogleFonts.robotoCondensed(
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                //user name or Email
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        controller: emailcontroller,
+                        textAlign: TextAlign.right,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: ' اسم المستخدم او البريد إلكتروني ',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                //password
 
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        controller: passwordcontroller,
+                        textAlign: TextAlign.right,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'كلمة المرور',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                //confim password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        controller: confirmpasswordcontroller,
+                        textAlign: TextAlign.right,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'تأكيد كلمة المرور',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                //sign in button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: GestureDetector(
+                    onTap: signUp,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          color: Colors.green[300],
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Center(
+                          child: Text(
+                        'التسجيل ',
+                        style: GoogleFonts.robotoCondensed(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      )),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                //text: sign up
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: openSignupScreen,
+                      child: Text(
+                        'سجل دخولك ',
+                        style: GoogleFonts.robotoCondensed(
+                            color: Colors.green[300],
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      'لديك حساب دخولك هنا؟',
+                      style: GoogleFonts.robotoCondensed(
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
