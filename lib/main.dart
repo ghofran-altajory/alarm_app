d6b96fb7d29fc8ccc8dfad937306979e35efec44
ghofran-altajory <167257184+ghofran-altajory@users.noreply.github.com>
2024-07-15 23:06:01 +0200

    ff

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alarm_app/screens/about_screen.dart';
+import 'package:alarm_app/screens/profile_screen.dart';
 import 'package:alarm_app/screens/tabs_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -30,7 +30,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: false,
       ),
-      home: const AboutScreen(),
+      home: const ProfileScreen(),
+      // const AboutScreen(),
       // const WelcomeScreen(), //const SettingScreen()
       // const TabsScreen(),
       //const SplashScreen(),
