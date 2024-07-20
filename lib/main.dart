b22d913267e04a0ab16e59e8777aa8422f262869
Ayafaraj20 <ayaehlees51@gmail.com>
2024-07-20 18:06:15 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
-import 'package:alarm_app/screens/about_screen.dart';
 import 'package:alarm_app/screens/admin_splash.dart';
-import 'package:alarm_app/screens/ads_screen.dart';
-<<<<<<< HEAD
-import 'package:alarm_app/screens/edit_password.dart';
-import 'package:alarm_app/screens/personal_data.dart';
-=======
->>>>>>> 20c36bd3c0e9534ece5dea443392c50bcce47a04
-import 'package:alarm_app/screens/setting_screen.dart';
 
-// ignore: unused_import
-import 'package:alarm_app/screens/subscribe_screen.dart';
 import 'package:alarm_app/screens/tabs_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -53,52 +43,47 @@ class _MyAppState extends State<MyApp> {
         // ],
         // child:
         MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: false,
-      ),
-<<<<<<< HEAD
-      // ignore: prefer_const_constructors
-      home: const AdminSplash()
-      
-      
-       // PersonalData()
-=======
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: false,
+            ),
+            home: const AdminSplash()
 
-      home: // PersonalData(),
-          //  const PaymentMethod(),
-          //CheckData(),
-          //const AdsScreen()
-          //const Notifications()
-          //const SplashScreen(),
-          //const EditProfileScreen()
-          //const SubscribeScreen()
-          // const ProfileScreen(),
-          // const AboutScreen(),
-          // const WelcomeScreen(),
-          // //const SettingScreen()
-          const TabsScreen(),
+            // PersonalData()
 
-      //const AdsScreen()
->>>>>>> 20c36bd3c0e9534ece5dea443392c50bcce47a04
-      // const SettingScreen()
+            // PersonalData(),
+            //  const PaymentMethod(),
+            //CheckData(),
+            //const AdsScreen()
+            //const Notifications()
+            //const SplashScreen(),
+            //const EditProfileScreen()
+            //const SubscribeScreen()
+            // const ProfileScreen(),
+            // const AboutScreen(),
+            // const WelcomeScreen(),
+            // //const SettingScreen()
+            // const TabsScreen(),
 
-      //const EditPassword(),
+            //const AdsScreen()
+            // const SettingScreen()
 
-      // const AboutScreen(),
-      //PersonalData(),
-      //CheckData(),
+            //const EditPassword(),
 
-      //const Notifications()
-      //const SplashScreen(),
-      //const EditProfileScreen()
-      //const SubscribeScreen()
-      // const ProfileScreen(),
+            // const AboutScreen(),
+            //PersonalData(),
+            //CheckData(),
 
-      // const WelcomeScreen(),
-      // //const SettingScreen()
-      // const TabsScreen(),
-    );
+            //const Notifications()
+            //const SplashScreen(),
+            //const EditProfileScreen()
+            //const SubscribeScreen()
+            // const ProfileScreen(),
+
+            // const WelcomeScreen(),
+            // //const SettingScreen()
+            // const TabsScreen(),
+            );
     // );
   }
 }
