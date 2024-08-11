f8a91901ede6cdc4efa9cfeb60894f6640c252bc
ghofran-altajory <167257184+ghofran-altajory@users.noreply.github.com>
2024-08-11 21:34:42 +0200

    تعديل الارشيف

--- lib/screens/archive.dart
+++ lib/screens/archive.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:io';
@@ -13,26 +14,57 @@ class Archive extends StatefulWidget {
 class _ArchiveState extends State<Archive> {
   List<File?> images = [];
 
-
-  
   showOption(BuildContext context) {
     return showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: const Text('make a choice'),
+        title: Align(
+            alignment: Alignment.center,
+            child: Text(":اختر الطريقة",
+                style: GoogleFonts.almarai(
+                  color: const Color(0xFF1883DB),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ))),
         content: SingleChildScrollView(
-          child: Column(children: [
-            ListTile(
-              leading: const Icon(Icons.image),
-              title: const Text('Gallery'),
-              onTap: () => imageFormGallery(context),
-            ),
-            ListTile(
-              leading: const Icon(Icons.camera_alt_outlined),
-              title: const Text('Camera'),
-              onTap: () => imageFormCamera(context),
-            ),
-          ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(children: [
+              // Padding( padding: const EdgeInsets.symmetric(horizontal: 10)),
+              ListTile(
+                trailing: const Icon(
+                  Icons.camera_alt_outlined,
+                  color: Colors.blue,
+                ),
+                title: Text('الكاميرا',
+                    style: GoogleFonts.almarai(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold)),
+                leading: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onTap: () => imageFormCamera(context),
+              ),
+              ListTile(
+                title: Text('المعرض',
+                    style: GoogleFonts.almarai(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold)),
+                trailing: const Icon(
+                  Icons.image,
+                  color: Colors.blue,
+                ),
+                leading: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onTap: () => imageFormGallery(context),
+              ),
+            ]),
+          ),
         ),
       ),
     );
@@ -45,6 +77,8 @@ class _ArchiveState extends State<Archive> {
       // imageFile = File(image.path);
       images.add(File(image.path));
     });
+    // final ref = FirebaseStorage.instance.ref().child(path);
+    // ref.putFile(file);
     Navigator.pop(context);
   }
 
@@ -60,51 +94,82 @@ class _ArchiveState extends State<Archive> {
 
   @override
   Widget build(BuildContext context) {
- 
-    
     return Scaffold(
       backgroundColor: const Color(0xFFFCFCFf),
-      body: 
-      
-      SafeArea(
+      body: SafeArea(
           child: Container(
               padding: const EdgeInsets.all(20),
               child: Center(
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: <Widget>[
- Expanded(
-            child: ListView.builder(
-              // scrollDirection: Axis.horizontal,
-              itemCount: images.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Stack(
-                    children: [
-                      Image.file(File(images[index]!.path)),
-                      // Image.file(File(imageFile!.path)),
-                      Positioned(
-                          right: 2,
-                          top: 2,
-                          child: IconButton(
-                            icon: const Icon(
-                              Icons.delete_outline,
-                              color: Colors.red,
+                      Text(
+                        ':الأرشيف',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.almarai(
+                          color: const Color(0xFF1883DB),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          // scrollDirection: Axis.horizontal,
+                          itemCount: images.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Stack(
+                                children: [
+                                  Image.file(File(images[index]!.path)),
+                                  // Image.file(File(imageFile!.path)),
+                                  Positioned(
+                                      right: 2,
+                                      top: 2,
+                                      child: IconButton(
+                                        icon: const Icon(
+                                          Icons.delete_outline,
+                                          color: Colors.red,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            images.removeAt(index);
+                                          });
+                                        },
+                                      ))
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: 70,
+                              height: 55,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xFF1883DB),
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: IconButton(
+                                    onPressed: () => showOption(context),
+                                    icon: const Center(
+                                      child: Icon(Icons.camera_alt_outlined,
+                                          color: Colors.white, size: 45),
+                                    )),
+                              ),
                             ),
-                            onPressed: () {
-                              setState(() {
-                                images.removeAt(index);
-                              });
-                            },
-                          ))
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-
+                          ],
+                        ),
+                      ),
                       // Row(
                       //   mainAxisAlignment: MainAxisAlignment.end,
                       //   children: [
@@ -129,23 +194,8 @@ class _ArchiveState extends State<Archive> {
                       //     fontWeight: FontWeight.bold,
                       //   ),
                       // ),
-
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Color.fromARGB(255, 20, 36, 49),
-                        child: IconButton(
-                            onPressed:()=> showOption(context),
-                            
-                          
-                            icon: const Center(
-                              child: Icon(Icons.camera,
-                                  color: Colors.blue, size: 45),
-                            )),
-                      ),
-                    
                     ]),
               ))),
     );
   }
 }
-
